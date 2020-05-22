9394c9d426dd491feee610b4e851bba4c001ecc2
Abdulkerim Sari <a.saridevelop@gmail.com>
2020-05-23 00:51:53 +0200

    TabNavigator ohne Icons

--- App.js
+++ App.js
@@ -1,27 +1,38 @@
-import React, { Component } from 'react';
-import { 
-  StyleSheet, Text, View 
-} from 'react-native';
-import { DrawerNavigator } from 'react-navigation'
-import HomeScreen from './HomeScreen'
-import SettingsScreen from './SettingsScreen'
-import { Drawer } from 'native-base';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Hallo</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   );
 }
 
-const MyApp = DrawerNavigator(
-  {
-    Home: {
-      HomeScreen
-    }
-  }
-)
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
